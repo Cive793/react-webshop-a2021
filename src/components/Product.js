1fd48a77599485a9e29ab3013aba5dcbf0c138be
peterstastny <peter.stastny7@gmail.com>
2021-11-18 23:41:15 +0100

    up to date code

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+/* import { useState } from "react";
 export default function Product(props) {
   const initialAmount = 0;
 
@@ -16,7 +16,7 @@ export default function Product(props) {
   return (
     <article
       style={{
-        backgroundColor: props.soldOut ? "red" : "green",
+        backgroundColor: props.soldout ? "red" : "green",
       }}
     >
       <h2>{props.productdisplayname}</h2>
@@ -24,6 +24,44 @@ export default function Product(props) {
       <p>Items: {amount}</p>
       <button onClick={addItem}>Add</button>
       <button onClick={subtractItem}>Subtrct</button>
+    </article>
+  );
+}
+ */
+
+import { useState } from "react";
+export default function Product(props) {
+  const [amount, setAmount] = useState(0);
+  const color = props.soldout ? "red" : "green";
+
+  function minus() {
+    setAmount((oldAmount) => {
+      if (oldAmount > 0) {
+        return oldAmount - 1;
+      }
+      return 0;
+    });
+  }
+  function plus() {
+    setAmount((oldAmount) => oldAmount + 1);
+    props.addToBasket({
+      price: props.price,
+      productdisplayname: props.productdisplayname,
+      id: props.id,
+    });
+  }
+
+  return (
+    <article
+      style={{
+        backgroundColor: color,
+      }}
+    >
+      <h2>{props.productdisplayname}</h2>
+      <p>${props.price}</p>
+      <button onClick={minus}> - </button>
+      {amount}
+      <button onClick={plus}> + </button>
     </article>
   );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,11 @@
 import Product from "./Product";
 export default function ProductList(props) {
-  const mapped = props.products.map((product) => <Product {...product} />);
+  const mapped = props.products.map((product) => <Product addToBasket={props.addToBasket} key={product.id} {...product} />);
   return (
     <section>
-      <h1>Product List</h1>
+      <h1>Product List {props.break}</h1>
       <section className="ProductList">{mapped}</section>
     </section>
   );
 }
+//<Product {...props.product} />
